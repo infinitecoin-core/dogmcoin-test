7400f7778375277fa28e5832aee2992e7c56f439
infinitecoin-core <59826757+infinitecoin-core@users.noreply.github.com>
2024-09-02 21:08:53 +0800

    Update mining.cpp

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -1355,7 +1355,6 @@ static const CRPCCommand commands[] =
     { "mining",             "getblocktemplate",       &getblocktemplate,       true,  {"template_request"} },
     { "mining",             "submitblock",            &submitblock,            true,  {"hexdata","parameters"} },
 
-    { "mining",             "getauxblock",            &getauxblock,            true,  {"hash", "auxpow"} },
     { "mining",             "createauxblock",         &createauxblock,         true,  {"address"} },
     { "mining",             "submitauxblock",         &submitauxblock,         true,  {"hash", "auxpow"} },
 
